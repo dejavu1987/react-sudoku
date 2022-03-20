82d2f709505685e8372bb2e6ce41755195d21830
Anil Maharjan <1720245+dejavu1987@users.noreply.github.com>
2022-03-20 18:51:53 +0100

    Modes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
-import { FunctionComponent, useState } from 'react';
-import { Cell } from './Cell';
-import { getRandomBoard, getRandomSeed, getRow, oneto9 } from './lib/sudoku';
-import './styles.scss';
+import { FunctionComponent, useState } from "react";
+import { Cell } from "./Cell";
+import { getRandomBoard } from "./lib/sudoku";
+import "./styles.scss";
 
 const board = getRandomBoard();
 
 const App: FunctionComponent = () => {
   const [active, setActive] = useState<number>();
+  const [mode, setMode] = useState<"SELECT" | "REMOVE" | "CALCULATE">("SELECT");
 
   const onCellClick = (n: number) => {
     setActive(n);
@@ -14,7 +15,29 @@ const App: FunctionComponent = () => {
 
   return (
     <div className="App">
-      <h1>DuSoKu</h1>
+      <header className="header">
+        <h1>DuSoKu</h1>
+        <div className="mode">
+          <button
+            className={mode === "SELECT" ? "active" : ""}
+            onClick={() => setMode("SELECT")}
+          >
+            S
+          </button>
+          <button
+            className={mode === "REMOVE" ? "active" : ""}
+            onClick={() => setMode("REMOVE")}
+          >
+            X
+          </button>
+          <button
+            className={mode === "CALCULATE" ? "active" : ""}
+            onClick={() => setMode("CALCULATE")}
+          >
+            Q
+          </button>
+        </div>
+      </header>
       <div className="board">
         {board.map((row, r) => {
           return row.map((n, i) => (
