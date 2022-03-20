44764ecf6049b5a62132de22a9cf828fa51892a7
Anil Maharjan <1720245+dejavu1987@users.noreply.github.com>
2022-03-20 10:55:28 +0100

    Active State

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,44 @@
-import "./styles.scss";
+import { FunctionComponent, useState } from 'react';
+import './styles.scss';
 
-const Cell = ({ r, c, value }) => {
-  return <div className={`cell r-${r} c-${c} v-${value}`}>{value}</div>;
+type Seed = number[];
+
+type CellProps = {
+  r: number;
+  c: number;
+  value: number;
+  active?: boolean;
+  onClick: () => void;
 };
 
-const rotateRow = (seed, index) => {
+const one29 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const seed = one29.sort(() => Math.random() - 0.5);
+
+const Cell: FunctionComponent<CellProps> = ({
+  r,
+  c,
+  value,
+  active,
+  onClick,
+}) => {
+  return (
+    <div
+      className={`cell r-${r} c-${c} v-${value}${active ? ' active' : ''}`}
+      onClick={onClick}
+    >
+      {value}
+    </div>
+  );
+};
+
+const rotateRow = (seed: Seed, index: number) => {
   var rowData = [...seed];
   const moved = rowData.splice(0, index);
   rowData = [...rowData, ...moved];
   return rowData;
 };
-const getRow = (seed, row) => {
+
+const getRow = (seed: Seed, row: number) => {
   if (row === 1) return seed;
   if (row === 2) {
     return rotateRow(seed, 3);
@@ -39,19 +67,29 @@ const getRow = (seed, row) => {
   return seed;
 };
 
-export default function App() {
-  const one29 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-  const seed = one29.sort(() => Math.random() - 0.5);
+const App: FunctionComponent = () => {
+  const [active, setActive] = useState<number>();
+
+  const onCellClick = (n: number) => {
+    setActive(n);
+  };
   return (
     <div className="App">
       <h1>DuSoKu</h1>
       <div className="board">
         {one29.map((r) => {
           return getRow(seed, r).map((n, i) => (
-            <Cell r={r} c={i + 1} value={n} />
+            <Cell
+              r={r}
+              c={i + 1}
+              value={n}
+              active={active === n}
+              onClick={() => onCellClick(n)}
+            />
           ));
         })}
       </div>
     </div>
   );
-}
+};
+export default App;
